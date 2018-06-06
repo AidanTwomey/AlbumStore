bfcc1f8098a9f21a4f3fb4452409381981ce0f25
Aidan Twomey <aidantwomey@Aidans-MacBook-Pro.local>
2018-06-06 22:18:50 +0100

    adding lambda

---
+++ index.js
@@ -0,0 +1,26 @@
+var AWS = require('aws-sdk');
+var flatten = require('flat')
+var sqs = new AWS.SQS({region : 'eu-west-1'});
+
+exports.handler = (event, context, callback) => {
+    
+    var flatAlbum = flatten( JSON.parse(event.body));
+    var returnMessage = JSON.stringify(flatAlbum);
+    
+    var params = {
+      MessageBody: returnMessage,
+      QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/160534783289/flat-albums'
+    };
+    
+    sqs.sendMessage(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+
+    var response = {
+        "statusCode": 200,
+        "body": returnMessage,
+        "isBase64Encoded": false
+    };
+    callback(null, response);
+};
